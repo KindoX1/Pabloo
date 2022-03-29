e9710fd1870a432222e742baabeeb3147ff9cf10
FoorDoX <49598364+KindoX1@users.noreply.github.com>
2022-03-30 00:37:41 +0200

    MAJ 3
    Changement fond couleur + Association des BP CLICK + OPTION au couleur séléctionner .
    + 2-3 modif sur les couleurs des boutton [ Hover + couleur ]

--- pabloo/popup.js
+++ pabloo/popup.js
@@ -3,6 +3,7 @@ let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
+  optionColor.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
