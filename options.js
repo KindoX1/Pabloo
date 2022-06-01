5e0afe84f1f046ecf0e750e672fc45614d8a9abd
FoorDoX <49598364+KindoX1@users.noreply.github.com>
2022-06-01 15:28:11 +0200

    MAJ 7
    - Refonte complete de l'extension 
    - Le selecteur de couleur marche mais change que les <p> .
    - Ajustement des <div></div> ( voir console )

--- options.js
+++ options.js
@@ -2,18 +2,12 @@ let page = document.getElementById("colorWell");
 let selectedClassName = "current";
 
 
-colorPicker.addEventListener("input", updateFirst, false);
-colorPicker.addEventListener("change", watchColorPicker, false);
-
-function watchColorPicker(event) {
-  document.querySelectorAll("p").forEach(function(p) {
-    p.style.color = event.target.value;
-  });
-}
 var colorWell
 var defaultColor = "#0000ff";
 
 window.addEventListener("load", startup, false);
+
+/INITIALISATION/
 
 function startup() {
   colorWell = document.querySelector("#colorWell");
@@ -22,10 +16,22 @@ function startup() {
   colorWell.addEventListener("change", updateAll, false);
   colorWell.select();
 }
-function updateFirst(event) {
-  var backgroundColor = document.querySelector("backgroundColor");
 
-  if (backgroundColor) {
-    backgroundColor.style.color = event.target.value;
+/REACTION/
+
+function updateFirst(event) {
+  var p = document.querySelector("p");
+
+  if (p) {
+    p.style.color = event.target.value;
   }
 }
+
+/QUAND SELECTEUR FERMER : RIEN BOUGE/
+
+function updateAll(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
+}
+
